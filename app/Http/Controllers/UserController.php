d4866e00415990dd968d7f7dacff1f125464ebda
Bobby Wurtz <bobby.wurtz@corvisnutrition.com>
2020-08-29 15:00:19 -0400

    Bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,10 +30,11 @@ class UserController extends Controller
 
     public function create(Request $request)
     {
+        dd('here');
         $categories = Categories::select('id', 'name')->get();
 
         $authors = Author::select('id', 'first_name', 'last_name')->get();
-        dd('here');
+        
         return view('user.createReport', compact('categories', 'authors'));
     }
 
