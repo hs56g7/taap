50f8dbb5f30ffc66b9e0d338ac565771912933c8
Bobby Wurtz <bobby.wurtz@corvisnutrition.com>
2020-08-29 15:29:54 -0400

    Fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        dd('auth start');
         return redirect()->route('login');
         dd('auth middleware');
     }
