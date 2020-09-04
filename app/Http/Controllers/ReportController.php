c4387982a6b0538b5b136272bc1c7043cdbfb9b2
Bobby Wurtz <bobby.wurtz@corvisnutrition.com>
2020-09-04 09:30:56 -0400

    Fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -14,7 +14,6 @@ class ReportController extends Controller
 {
     public function index(Request $request)
     {
-        dfghj
         $category_ids = AuthorsAndReports::where('status', 1)->pluck('category_id');
         
         $categories = Categories::whereIn('id', $category_ids)->get();
