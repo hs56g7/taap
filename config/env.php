2c8710973c3c53a58acb19db163dd77820436aea
Bobby Wurtz <bobby.wurtz@corvisnutrition.com>
2020-09-06 11:39:48 -0400

    Meta tag update

--- config/env.php
+++ config/env.php
@@ -10,6 +10,7 @@ return [
     */
 
     'APP_ENV'                                   => env('APP_ENV'),
+    'APP_URL_SHORT'                             => env('APP_URL_SHORT'),
     'secret_key'                                => env('secret_key'),
 
 ];
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,7 +13,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>{{ $title ?? "TAAP"}}</title>
 <link rel="shortcut icon" href="{{ asset('img/TAAP_Favicon.svg') }}">
 <link rel="stylesheet" href="{{ asset('grandy/assets/css/master.css') }}">
 <link rel="stylesheet" href="{{ asset('grandy/assets/css/responsive.css') }}">
@@ -27,6 +26,25 @@
 <meta name="twitter:site" content="@taap2020" />
 <meta name="twitter:title" content="TAAP" />
 <meta name="twitter:description" content="{{ $description ?? 'We are a group of law students based at Duke University documenting instances where the Trump Administration may have violated the law. Accountability matters, and no one is above the law.' }}" />
-<meta name="twitter:image" content="{{ asset('img/TAAP_FullLogo.png') }}" />
+<meta name="twitter:image" content="{{ asset('img/TAAP_FullLogo.svg') }}" />
+
+<!-- Primary Meta Tags -->
+<title>{{ $title ?? "TAAP"}}</title>
+<meta name="title" content="{{ $title ?? 'TAAP'}}">
+<meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ $report_url ?? config('env.APP_URL_SHORT') }}">
+<meta property="og:title" content="{{ $title ?? 'TAAP'}}">
+<meta property="og:description" content="{{ $description ?? 'We are a group of law students based at Duke University documenting instances where the Trump Administration may have violated the law. Accountability matters, and no one is above the law.' }}">
+<meta property="og:image" content="{{ asset('img/TAAP_FullLogo.svg') }}">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="{{ $report_url ?? config('env.APP_URL_SHORT') }}">
+<meta property="twitter:title" content="{{ $title ?? 'TAAP'}}">
+<meta property="twitter:description" content="{{ $description ?? 'We are a group of law students based at Duke University documenting instances where the Trump Administration may have violated the law. Accountability matters, and no one is above the law.' }}">
+<meta property="twitter:image" content="{{ asset('img/TAAP_FullLogo.svg') }}">
 
 </head>
